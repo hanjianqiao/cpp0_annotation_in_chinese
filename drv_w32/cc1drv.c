a9abbb7dad948f04dbf684a92862156ea4bc5f22
hanjianqiao <hanjianqiao@gmail.com>
2014-09-03 19:15:11 +0800

    Add the source code.

---
+++ drv_w32/cc1drv.c
@@ -0,0 +1,41 @@
+typedef unsigned char UCHAR;
+
+typedef struct GO_STR_FILE {
+	UCHAR *p0, *p1, *p;
+	int dummy;
+} GO_FILE;
+
+extern GO_FILE GO_stdin, GO_stdout, GO_stderr;
+extern struct GOL_STR_MEMMAN GOL_memman, GOL_sysman;
+UCHAR *GOL_work0;
+
+struct bss_alloc {
+	UCHAR _stdout[SIZ_STDOUT];
+	UCHAR _stderr[SIZ_STDERR];
+	UCHAR syswrk[SIZ_SYSWRK];
+	UCHAR work[SIZ_WORK];
+	UCHAR for_align[16];
+};
+
+void GOL_sysabort(UCHAR termcode);
+void *GOL_memmaninit(struct GOL_STR_MEMMAN *man, unsigned int size, void *p);
+void *GOL_sysmalloc(unsigned int size);
+void GOL_callmain0();
+
+void mainCRTStartup(void)
+/* Sure, I put the -o options */
+/* Here, -o option is stripped */
+/* But (because the size can not be measured in the standard input) The name of the input file write */
+{
+	struct bss_alloc bss_image;
+	struct bss_alloc *bss0 = (void *) ((((int) &bss_image) + 0x0f) & ~0x0f);
+	GO_stdout.p0 = GO_stdout.p = bss0->_stdout;
+	GO_stdout.p1 = GO_stdout.p0 + SIZ_STDOUT;
+	GO_stdout.dummy = ~0;
+	GO_stderr.p0 = GO_stderr.p = bss0->_stderr;
+	GO_stderr.p1 = GO_stderr.p0 + (SIZ_STDERR - 128); /* I keep a little smaller on purpose */
+	GO_stderr.dummy = ~0;
+	GOL_memmaninit(&GOL_sysman, SIZ_SYSWRK, bss0->syswrk);
+	GOL_memmaninit(&GOL_memman, SIZ_WORK, GOL_work0 = bss0->work);
+	GOL_callmain0();
+}
---
+++ drv_w32/others.c
@@ -0,0 +1,39 @@
+/* for w32 */
+
+//	void GOLD_exit(int s);		/* Exit */
+int GOLD_getsize(const UCHAR *name); /* File size acquisition */
+int GOLD_read(const UCHAR *name, int len, UCHAR *b0);
+	/* Read file, binary mode,
+		You have been file check just before the caller the size,
+		I come to request the file size perfect */
+
+int GOLD_getsize(const UCHAR *name)
+{
+	HANDLE h;
+	int len = -1;
+	h = CreateFileA((char *) name, GENERIC_READ, FILE_SHARE_READ,
+		NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+	if (h == INVALID_HANDLE_VALUE)
+		goto err;
+	len = GetFileSize(h, NULL);
+	CloseHandle(h);
+err:
+	return len;
+}
+
+int GOLD_read(const UCHAR *name, int len, UCHAR *b0)
+{
+	HANDLE h;
+	int i;
+	h = CreateFileA((char *) name, GENERIC_READ, FILE_SHARE_READ,
+		NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+	if (h == INVALID_HANDLE_VALUE)
+		goto err;
+	ReadFile(h, b0, len, &i, NULL);
+	CloseHandle(h);
+	if (len != i)
+		goto err;
+	return 0;
+err:
+	return 1;
+}
